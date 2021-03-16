2caf59b0835692701ce977bd4f75d4e4668223e1
LAPTOP-0261QBGC\nikun <nikunjsaraf2001@gmail.com>
2021-03-16 16:58:26 +0530

    Added questions and answers

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,20 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answers = [
+    false,
+    true,
+    true,
+  ];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -63,7 +77,19 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionNumber];
+
+                if (correctAnswer) {
+                  print('Correct answer');
+                } else {
+                  print('Wrong answer!');
+                }
                 //The user picked true.
+                setState(
+                  () {
+                    questionNumber++;
+                  },
+                );
               },
             ),
           ),
@@ -84,12 +110,48 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionNumber];
+
+                if (!correctAnswer) {
+                  print('Correct answer');
+                } else {
+                  print('Wrong answer!');
+                }
                 //The user picked false.
+                setState(
+                  () {
+                    questionNumber++;
+                  },
+                );
               },
             ),
           ),
         ),
-        
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(
+              Icons.check,
+              color: Colors.greenAccent,
+            ),
+            Icon(
+              Icons.close_sharp,
+              color: Colors.redAccent,
+            ),
+            Icon(
+              Icons.close_sharp,
+              color: Colors.redAccent,
+            ),
+            Icon(
+              Icons.close_sharp,
+              color: Colors.redAccent,
+            ),
+            Icon(
+              Icons.close_sharp,
+              color: Colors.redAccent,
+            ),
+          ],
+        ),
       ],
     );
   }
